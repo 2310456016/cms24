c67ef0eb16b85a0e936e204f6ce7b8f00a5bfe43
Melanie Ramer <melanieramer.mr@gmail.com>
2024-12-27 21:06:43 +0100

    Commit

---
+++ packages/mythologie/Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('TYPO3') or die('Access denied.');
+
+call_user_func(function()
+{
+    /**
+     * Temporary variables
+     */
+    $extensionKey = 'mythologie';
+
+    /**
+     * Default TypoScript for Mythologie
+     */
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+        $extensionKey,
+        'Configuration/TypoScript',
+        'mythologie'
+    );
+});
